7f0ac2f94008e1d46abc7f33e8489b0a3a241d71
stephen <stevedogbey@gmail.com>
2022-06-20 10:18:36 +0000

    commit tree 4efadbf381276645c625aa0b7aceb4dbaa98acb1

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const newCat = [...cats, name];
+    return newCat;
+}
+
+function prependCat(name){
+    const myCats =[name, ...cats];
+    return myCats;
+}
+function removeLastCat(){
+    const rmLast = [...cats.slice(0,-1)]
+    return rmLast;
+}
+function removeFirstCat(){
+    const rmFirst = [...cats.slice(1)]
+    return rmFirst;
+}
